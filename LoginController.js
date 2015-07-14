9196de2c3569a6025d24a9df8c8a33caeea86b6e
Simon Mor <simomor@gmail.com>
2015-07-14 23:09:31 +0300

    version 1.2.1 db demo on client

--- LoginController.js
+++ LoginController.js
@@ -3,15 +3,13 @@
 
 	var LoginController = function($scope, $location, cindy) {
 		$scope.userLogin = function(user) {
-			console.log(user);
-			var url = "someUrl";
-			var onUserLoginSuccess = function(response) {
+			$scope.error = null;
+			if (user.name == "master" && user.password == "yoda") {
 				$location.path("/user/" + user.name);
-			};
-			var onUserLoginError = function(reason) {
-				$location.path("/user/" + user.name);
-			};
-			cindy.ping(url, user).then(onUserLoginSuccess, onUserLoginError);
+			}
+			else {
+				$scope.error = true;
+			}
 		};
 
 	};
